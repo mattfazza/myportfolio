84706356f361e24da442552123c6dc4a7822f28a
mattfazza <mathewsfazza@gmail.com>
2018-12-09 23:42:09 -0600

    Started adding styling to the landing page
     to fix: the icons and image display

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class LandingPage extends Component {
 
     render(){
         return(
-            <div>
-                <h1>landingpage</h1>
+            <div style={{ width: "100%", margin: "auto"}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                            src="/Users/MattFazza/PersonalProjects/Angular/React-Trainning/myportfolio/img/linkedin.jpg.sb-b77734d5-DlUrmh"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Full Stack Engineer</h1>
+
+                            <hr />
+
+                            <p>Mongo | Express | React | Node </p>
+
+                            <div className="socialLinks">
+                                <a href="http://www.google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linked-square" area-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
