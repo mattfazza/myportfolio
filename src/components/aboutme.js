2afe060f942cd42fde2929a2ee234670d52ce1c5
mattfazza <mathewsfazza@gmail.com>
2018-12-21 21:21:27 -0600

    Fixed aboutme.js to display the landing page
    Still need to make this better, as they're redundant in routing.

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import LandingPage from "./landingpage";
 
 class AboutMe extends Component {
 
     render(){
         return(
             <div>
-                <h1>AboutMe</h1>
+                <LandingPage/>
             </div>
         )
     }
